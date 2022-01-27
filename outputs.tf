223e4c54b2ddb89135ad3d50868850e7c517ec55
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-01-27 16:42:22 +0530

    changes in output

--- outputs.tf
+++ outputs.tf
@@ -38,23 +38,8 @@ output "type" {
 output "mongo_pw" {
   value       = var.mongo_password
   description = "mongo admin pw"
-  depends_on  = [
-    null_resource.deploy_instance
-  ]
-}
-
-output "mongo_namespace" {
-  value       = var.mongo_namespace
-  description = "Namespace mongo is located in cluster"
-  depends_on  = [
-    null_resource.deploy_instance
-  ]
 }
 
 output "mongo_servicename" {
   description = "Name of mongo service to connect to"
-  depends_on  = [
-    null_resource.deploy_instance
-  ]
-  value       = data.local_file.svcname.content
 }
--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -12,6 +12,9 @@ resource null_resource write_outputs {
         layer       = module.gitops_module.layer
         layer_dir   = module.gitops_module.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
         type        = module.gitops_module.type
+        mongo_password = module.gitops_module.mongo_pw
+        mongo_servicename = module.gitops_module.mongo_servicename
+
       })
     }
   }
