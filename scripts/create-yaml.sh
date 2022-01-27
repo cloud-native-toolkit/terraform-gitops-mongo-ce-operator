1ac57af61c1e6695f05b083bf52c3b7464f134a8
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-01-27 19:05:24 +0530

    added mkdir

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -5,7 +5,7 @@ MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 CHART_DIR=$(cd "${MODULE_DIR}/chart/${NAME}"; pwd -P)
 NAME="$1"
 DEST_DIR="$2"
-
+mkdir -p "${DEST_DIR}"
 ## Add logic here to put the yaml resource content in DEST_DIR
 cp -R "${CHART_DIR}"/* "${DEST_DIR}"
 
