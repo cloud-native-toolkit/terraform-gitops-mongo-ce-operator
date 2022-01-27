5f70bc66594593b23e708c25d39c2c1c8e8c21e9
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-01-27 18:58:55 +0530

    charts folder restructuring

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -2,10 +2,14 @@
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
-
+CHART_DIR=$(cd "${MODULE_DIR}/chart/${NAME}"; pwd -P)
 NAME="$1"
 DEST_DIR="$2"
 
 ## Add logic here to put the yaml resource content in DEST_DIR
+cp -R "${CHART_DIR}"/* "${DEST_DIR}"
 
+if [[ -n "${VALUES_FILE}" ]] && [[ -n "${VALUES_CONTENT}" ]]; then
+  echo "${VALUES_CONTENT}" > "${DEST_DIR}${VALUES_FILE}"
+fi
 find "${DEST_DIR}" -name "*"
