2c3c40724fc3039fe79d40e1ac371e4e23293b20
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-01-31 14:57:23 +0530

    changes in create-yaml

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -8,8 +8,8 @@ DEST_DIR="$2"
 mkdir -p "${DEST_DIR}"
 ## Add logic here to put the yaml resource content in DEST_DIR 
 cp -R "${CHART_DIR}"/* "${DEST_DIR}"
-
-if [[ -n "${VALUES_FILE}" ]] && [[ -n "${VALUES_CONTENT}" ]]; then
+##if [[ -n "${VALUES_FILE}" ]] && [[ -n "${VALUES_CONTENT}" ]]; then
+if  [[ -n "${VALUES_CONTENT}" ]]; then
   echo "${VALUES_CONTENT}" > "${DEST_DIR}${VALUES_FILE}"
 fi
 find "${DEST_DIR}" -name "*"
