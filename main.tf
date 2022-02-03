588e4c1f59805465df7dde743b3c43ee7240ea9e
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-02-03 17:16:38 +0530

    changes

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ locals {
       namespace= var.namespace
       mongocesecret = {
           password= var.mongo_password
-          crt =  filebase64("${local.tmp_dir}/server.crt")
-          key = filebase64("${local.tmp_dir}/server.key")
+          crt =  filebase64("${srvcrtfile.sensitive_content}")
+          key = filebase64("${srvkeyfile.sensitive_content}")
 
         }
       mongocemongodbcommunity = {
