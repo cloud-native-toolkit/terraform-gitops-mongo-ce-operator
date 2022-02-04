289cca35c1356f2725f20970d4ad4fd9a164800f
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-02-04 12:33:42 +0530

    removed destroy

--- main.tf
+++ main.tf
@@ -209,17 +209,6 @@ resource null_resource setup_gitops_destroy {
     }
   }
 
-  provisioner "local-exec" {
-    when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
 }
-
-
 
 
