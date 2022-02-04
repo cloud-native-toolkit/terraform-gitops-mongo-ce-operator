1c367addd48cc7d60dca14eb4c628902da578e34
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-02-04 12:45:31 +0530

    totally removed destroy

--- main.tf
+++ main.tf
@@ -185,30 +185,7 @@ module "service_account" {
 }
 
 
-resource null_resource setup_gitops_destroy {
-  depends_on = [null_resource.create_yaml]
 
-  triggers = {
-    name = local.name
-    namespace = var.namespace
-    yaml_dir = local.yaml_dir
-    server_name = var.server_name
-    layer = local.layer
-    type = local.type
-    git_credentials = yamlencode(var.git_credentials)
-    gitops_config   = yamlencode(var.gitops_config)
-    bin_dir = local.bin_dir
-  }
 
-  provisioner "local-exec" {
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-
-}
 
 
