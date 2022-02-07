4489a7f0c0d5d35c4d2b77c64d841c69598d5875
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-02-07 09:24:31 +0530

    changed setup gitops command

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
   name          = "mongo-ce"
   bin_dir       = module.setup_clis.bin_dir
-  yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
+  yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart"
   service_url   = "http://${local.name}.${var.namespace}"
   tmp_dir = "${path.cwd}/.tmp/${local.name}"
   values_content = {  
@@ -30,6 +30,7 @@ locals {
   application_branch = "main"
   namespace = var.namespace
   layer_config = var.gitops_config[local.layer]
+  values_file = "values-${var.server_name}.yaml"
 }
 
 module setup_clis {
@@ -224,7 +225,7 @@ resource null_resource setup_gitops {
   //depends_on = [null_resource.create_yaml,module.service_account]
 
   provisioner "local-exec" {
-    command = "${local.bin_dir}/igc gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}' --type '${local.type}' --debug"
+    command = "${local.bin_dir}/igc gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}' --type '${local.type}' --valueFiles 'values.yaml,${local.values_file}' --debug"
 
     environment = {
       GIT_CREDENTIALS = yamlencode(nonsensitive(var.git_credentials))
