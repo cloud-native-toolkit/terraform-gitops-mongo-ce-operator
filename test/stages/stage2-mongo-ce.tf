646d4ea37ea90921f8b943f4acbc8b3dbd360a23
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-02-10 23:40:06 -0600

    Cleans up variables
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage2-mongo-ce.tf
+++ test/stages/stage2-mongo-ce.tf
@@ -1,7 +1,6 @@
 module "gitops_module" {
   source = "./module"
 
-  cluster_config_file = module.dev_cluster.config_file_path
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
